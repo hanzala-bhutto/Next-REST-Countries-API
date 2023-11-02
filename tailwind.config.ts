03bc0df1111f9eae0a602e19697b2f10a919c75a
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-02 18:49:44 +0500

    challenge started

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        darkBlueDark: 'hsl(209, 23%, 22%)',
+        veryDarkBlueDark: 'hsl(207, 26%, 17%)',
+        veryDarkBlueLight: ' hsl(200, 15%, 8%)',
+        veryLightGrayDark: 'hsl(0, 0%, 52%)',
+        darkGrayLight: 'hsl(0, 0%, 98%)',
+        white: 'hsl(0, 0%, 100%)'
       },
+
+
     },
   },
   plugins: [],
