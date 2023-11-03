f4c58ba3e603c846e343a0ced832146b3add10b8
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-03 11:38:45 +0500

    search design and searchItem completed

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,33 +2,73 @@
 import { useEffect, useState } from "react";
 import { CountryCard } from "@/components";
 import Link from "next/link";
+import { Searchbar } from "@/components/Searhbar";
+import { Filter } from "@/components/Filter";
+
+interface Country { 
+    name: string;
+    population: number;
+    region: string;
+    capital: string;
+    flagImg: string;
+}
 
 export default function Home(){
 
-    const [countries, setCountries] = useState([]);
+    const [countries, setCountries] = useState<Country[]>([]);
+    const [filteredCountries, setFilteredCountries] = useState<Country[]>([]);
     
     useEffect(() => {
         fetch('http://localhost:3000/api/countries')
         .then((data) =>  data.json())
         .then((data) => {
             setCountries(data.shapeData);
+            setFilteredCountries(data.shapeData);
+
             console.log(data);
             console.log(countries)
         });
     },[]);
 
+    const search = (searchItem: string) => {
+        if (searchItem === ''){
+            setFilteredCountries(countries);
+            return;
+        }
+        const filteredCountries = countries.filter((country: Country) => {
+            return country?.name.toLowerCase().includes(searchItem.toLowerCase());
+        });
+        setFilteredCountries(filteredCountries);
+    }
+
+    const filter = (filterItem: string) => {
+        if (filterItem === ''){
+            setFilteredCountries(countries);
+            return;
+        }
+        const filteredCountries = countries.filter((country: Country) => {
+            return country?.region.toLowerCase().includes(filterItem.toLowerCase());
+        });
+        setFilteredCountries(filteredCountries);
+    }
+
     return (
-            <section className="flex flex-row flex-wrap gap-12">
-                {countries.length
+        <>
+            <section className="flex flex-row flex-wrap gap-8 justify-between mb-10">
+                <Searchbar search={search}/>
+                <Filter filter={filter}/>
+            </section>
+            <section className="flex flex-row flex-wrap gap-[3.3rem] mb-12">
+                {filteredCountries.length
                 ?
-                countries.map((country,index) => {
+                filteredCountries.map((country,index) => {
                     return (
                     <Link href={`/details`} key={index}><CountryCard key={index} country={country}/></Link>
                     )
                 })
                 :
-                <p>Loading...</p>}
+                <p>No Countries to Show !</p>}
             </section>
-
+        </>
     )
 }
