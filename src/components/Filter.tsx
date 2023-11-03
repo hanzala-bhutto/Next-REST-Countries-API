9f85b0c523b4383c8a2b950d8377bc18911410f0
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-03 11:39:01 +0500

    filter Design and filter Items Completed

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,6 +1,23 @@
+import { Dropdown } from "flowbite-react";
 
-export const Filter = () => {
+interface FilterProps {
+    filter: (filterItem: string) => void;
+
+}
+
+export const Filter = ({filter}: FilterProps) => {
+
+    const onSelect = (item: string) => {
+        filter(item);
+    }
+
     return (
-        <></>
+        <Dropdown class="w-48 rounded-xl border border-gray-300 bg-white dark:border-gray-600 dark:bg-darkBlueDark dark:text-white" label="Filter by Region" size="lg">
+        <Dropdown.Item onClick={() => onSelect('Africa')}>Africa</Dropdown.Item>
+        <Dropdown.Item onClick={() => onSelect('America')}>America</Dropdown.Item>
+        <Dropdown.Item onClick={() => onSelect('Asia')}>Asia</Dropdown.Item>
+        <Dropdown.Item onClick={() => onSelect('Europe')}>Europe</Dropdown.Item>
+        <Dropdown.Item onClick={() => onSelect('Oceania')}>Oceania</Dropdown.Item>
+        </Dropdown>   
     );
 }
