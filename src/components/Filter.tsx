d7aa714a39015a239ae482e7d4101f97ed1981dd
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-03 11:48:05 +0500

    filter class to className

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -12,7 +12,7 @@ export const Filter = ({filter}: FilterProps) => {
     }
 
     return (
-        <Dropdown class="w-48 rounded-xl border border-gray-300 bg-white dark:border-gray-600 dark:bg-darkBlueDark dark:text-white" label="Filter by Region" size="lg">
+        <Dropdown className="w-48 rounded-xl border border-gray-300 bg-white dark:border-gray-600 dark:bg-darkBlueDark dark:text-white" label="Filter by Region" size="lg">
         <Dropdown.Item onClick={() => onSelect('Africa')}>Africa</Dropdown.Item>
         <Dropdown.Item onClick={() => onSelect('America')}>America</Dropdown.Item>
         <Dropdown.Item onClick={() => onSelect('Asia')}>Asia</Dropdown.Item>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
 
   return (
     <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-darkBlueDark dark:border-gray-700">
-      <div className="px-10 py-4 lg:px-20">
+      <div className="px-5 py-4 sm:px-20">
 
           <div className="flex justify-between items-center">
             <h1 className="text-md font-bold text-center sm:text-lg  text-gray-800 dark:text-white">
